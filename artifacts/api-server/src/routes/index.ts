d31542a81a71ad9370890ba3f4586fcd6c192d28
Brandon1212b <111893060+Brandon1212b@users.noreply.github.com>
2026-06-29 19:01:02 -0400

    Add oddsRouter import to index.ts

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -1,7 +1,3 @@
-diff --git a/artifacts/api-server/src/routes/index.ts b/artifacts/api-server/src/routes/index.ts
-index de5299e..0000000 100644
---- a/artifacts/api-server/src/routes/index.ts
-+++ b/artifacts/api-server/src/routes/index.ts
 @@
  import smackboardRouter from "./smackboard";
  import adminRouter from "./admin";
